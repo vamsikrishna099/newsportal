fc6e26827a5b942c9d0bc8a4f4c4fb1d43952fc6
vamsikrishna099 <168620364+vamsikrishna099@users.noreply.github.com>
2024-05-18 17:52:46 +0530

    task1 completed successfully

---
+++ core/src/main/java/com/gofortrainings/newsportal/core/models/InThisCategory.java
@@ -0,0 +1,16 @@
+package com.gofortrainings.newsportal.core.models;
+
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+@Model(adaptables = {Resource.class, SlingHttpServletRequest.class},defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public class InThisCategory {
+    @ValueMapValue
+    private String title;
+
+    
+
+}
